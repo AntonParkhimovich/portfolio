19c9c4f97a35b8cddd985ba719822d1bbb4a0549
Anton Parkhimovich <pad1907@gmail.com>
2021-09-21 01:22:34 +0300

    feat: add hello section

--- portfolio/src/components/HelloSection.js
+++ portfolio/src/components/HelloSection.js
@@ -1,7 +1,14 @@
-const HelloSection=()=>{
+import { useContext } from "react"
+import LanguageContext from "../contextLanguage"
+
+export const HelloSection=()=>{
+    const [language] = useContext(LanguageContext)
     return(
         <section className={'hello-section'}>
-            <h2 className={'hello-text'}></h2>
+            <div className={'hello-section__main'}>
+                <h2 className={'hello-section__title'}>{language.helloSection.title}</h2>
+                <h2 className={'hello-section__subtitle'}>{language.helloSection.subtitle}</h2>
+            </div>
         </section>
     )
 }
