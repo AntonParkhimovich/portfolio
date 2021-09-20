d9953585afc852083b65d595e33b843c9ab52961
Anton Parkhimovich <pad1907@gmail.com>
2021-09-20 22:00:56 +0300

    feat: add new portfolio idea

---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,12 @@
+
+export const Header =( )=>{
+    return(
+        <header className={'header'}>
+            <nav className={'navigation'}>
+                <ul className={'navigation-link'}>
+                    
+                </ul>
+            </nav>
+        </header>
+    )
+}
