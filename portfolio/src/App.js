7f533f3e276374b1f26ad69d826361ac7746facc
Anton Parkhimovich <pad1907@gmail.com>
2021-09-20 23:20:15 +0300

    feat: add header

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 import './styles/main.scss'
 import { Header } from './components/Header';
 
 function App() {
   return (
+    <Router>
    <Header/>
+   </Router>
   );
 }
 
--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,10 +1,15 @@
-
+import {
+    NavLink
+  } from "react-router-dom";
 export const Header =()=>{
     return(
         <header className={'header'}>
             <nav className={'navigation'}>
-                <ul className={'navigation-link'}>
-                    
+                <ul className={'navigation-items'}>
+                    <NavLink className={'navigation-item'} activeClassName={'active'} to="/">Home</NavLink>
+                    <NavLink className={'navigation-item'} activeClassName={'active'} to="/about">About</NavLink>
+                    <NavLink className={'navigation-item'} activeClassName={'active'} to="portfolio">Portfolio</NavLink>
+                    <NavLink className={'navigation-item'} activeClassName={'active'} to="contact">Contact me</NavLink>
                 </ul>
             </nav>
         </header>
