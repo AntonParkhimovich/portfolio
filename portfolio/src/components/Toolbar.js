dc603ef1bc97a1a9c1aafd0f27d79236d5cb8fd0
Anton Parkhimovich <pad1907@gmail.com>
2021-11-09 16:46:38 +0300

    feat: add counter procents

--- portfolio/src/components/Toolbar.js
+++ portfolio/src/components/Toolbar.js
@@ -1,11 +1,26 @@
+import { useState, useEffect} from "react";
+
 export const Toolbar = ({ text, procents }) => {
+  const [countedProcents, setCounted] = useState(0)
+ 
+  useEffect(() => {
+    let timerId
+      if(countedProcents< procents){
+       timerId = setTimeout(()=>setCounted(countedProcents+1),10)
+      }
+      return ()=>{
+        if(timerId!==undefined){
+          clearTimeout(timerId)
+        }
+      }
+  },[countedProcents]);
   return (
     <div className={"about-skills__toolbar"}>
-          <div style={{ width: procents }} className={"about-skills__fill"}>
+          <div style={{ width: `${countedProcents}%` }} className={"about-skills__fill"}>
               <div className={'about-skills__fill-paint'}></div>
               <div className={"about-skills__text"}>{text}</div>
           </div>
-          <span className={"about-skills__procents"}>{procents}</span>
+          <span className={"about-skills__procents"}>{countedProcents}%</span>
     </div>
   );
 };
--- portfolio/src/skillsData.js
+++ portfolio/src/skillsData.js
@@ -1,8 +1,8 @@
 export const skills =[
-    {text:'HTML', procent:'60%'},
-    {text:'CSS', procent:'50%'},
-    {text:'Javssript', procent:'60%'},
-    {text:'React', procent:'70%'},
-    {text:'Git', procent:'40%'},
-    {text:'Sass', procent:'70%'}
+    {text:'HTML', procent:'60'},
+    {text:'CSS', procent:'50'},
+    {text:'Javssript', procent:'60'},
+    {text:'React', procent:'70'},
+    {text:'Git', procent:'40'},
+    {text:'Sass', procent:'70'}
 ]
